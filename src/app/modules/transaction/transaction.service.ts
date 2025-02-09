a2f4ddba66e1045f7bdec26a603a005480fe0fa1
manik <maniksarker652@gmail.com>
2025-02-09 15:05:21 +0600

    create function for get all and my transaction get

---
+++ src/app/modules/transaction/transaction.service.ts
@@ -0,0 +1,49 @@
+import QueryBuilder from '../../builder/QueryBuilder';
+import Transaction from './transaction.model';
+
+const getAllTransaction = async (query: Record<string, unknown>) => {
+  const transactionQuery = new QueryBuilder(Transaction.find(), query)
+    .search([''])
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const meta = await transactionQuery.countTotal();
+  const result = await transactionQuery.modelQuery;
+  return {
+    meta,
+    result,
+  };
+};
+
+const getMyTransaction = async (
+  profileId: string,
+  query: Record<string, unknown>,
+) => {
+  const transactionQuery = new QueryBuilder(
+    Transaction.find({
+      $or: [{ paymentReceiver: profileId }, { paymentSender: profileId }],
+    }),
+    query,
+  )
+    .search([''])
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const meta = await transactionQuery.countTotal();
+  const result = await transactionQuery.modelQuery;
+  return {
+    meta,
+    result,
+  };
+};
+
+const TransactionService = {
+  getAllTransaction,
+  getMyTransaction,
+};
+
+export default TransactionService;
