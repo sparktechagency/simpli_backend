2b7c14c4c856dd1db7cac2e84f76e191e9327861
manik <maniksarker652@gmail.com>
2025-02-11 11:05:42 +0600

    create functions for stripe controllers for update and create onboaridng link

---
+++ src/app/modules/stripe/stripe.controller.ts
@@ -0,0 +1,32 @@
+import catchAsync from '../../utilities/catchasync';
+import sendResponse from '../../utilities/sendResponse';
+import StripeService from './stripe.service';
+
+const createOnboardingLink = catchAsync(async (req, res) => {
+  const result = await StripeService.createConnectedAccountAndOnboardingLink(
+    req.user,
+  );
+
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: 'Link created successfully',
+    data: result,
+  });
+});
+const updateOnboardingLink = catchAsync(async (req, res) => {
+  const result = await StripeService.updateOnboardingLink(req.user.id);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: 'Link updated successfully',
+    data: result,
+  });
+});
+
+const stripeController = {
+  createOnboardingLink,
+  updateOnboardingLink,
+};
+
+export default stripeController;
