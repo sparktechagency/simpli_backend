10f806a98c961dd71aeb8b119c2e37375b32cefd
manik <maniksarker652@gmail.com>
2025-02-25 16:53:42 +0600

    create model for order

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,48 @@
+import { model, Schema } from 'mongoose';
+import { IOrder, IOrderItem } from './order.interface';
+import {
+  ENUM_DELIVERY_STATUS,
+  ENUM_PAYMENT_METHOD,
+  ENUM_PAYMENT_STATUS,
+} from '../../utilities/enum';
+
+const OrderItemSchema: Schema = new Schema<IOrderItem>({
+  product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+  variant: { type: Schema.Types.ObjectId, ref: 'Variant', default: null },
+  quantity: { type: Number, required: true, min: 1 },
+  price: { type: Number, required: true, min: 0 },
+});
+
+const OrderSchema: Schema = new Schema<IOrder>(
+  {
+    reviewer: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    bussiness: {
+      type: Schema.Types.ObjectId,
+      ref: 'Bussiness',
+      required: true,
+    },
+    items: { type: [OrderItemSchema], required: true },
+    totalQuantity: { type: Number, required: true, min: 1 },
+    subTotal: { type: Number, required: true, min: 0 },
+    deliveryFee: { type: Number, required: true, min: 0 },
+    totalPrice: { type: Number, required: true, min: 0 },
+    paymentMethod: {
+      type: String,
+      enum: Object.values(ENUM_PAYMENT_METHOD) as string[],
+      required: true,
+    },
+    paymentStatus: {
+      type: String,
+      enum: Object.values(ENUM_PAYMENT_STATUS) as string[],
+      required: true,
+    },
+    deliveryStatus: {
+      type: String,
+      enum: Object.values(ENUM_DELIVERY_STATUS) as string[],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Order = model<IOrder>('Order', OrderSchema);
