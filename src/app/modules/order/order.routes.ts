e5bae8603e23594b9f5ad86a77e763ea04211906
manik <maniksarker652@gmail.com>
2025-02-25 17:32:39 +0600

    create api for order

--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import auth from '../../middlewares/auth';
 import { USER_ROLE } from '../user/user.constant';
 import OrderController from './order.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import OrderValidations from './order.validation';
 
 const router = express.Router();
 
 router.post(
   '/create-order',
   auth(USER_ROLE.reviewer),
+  validateRequest(OrderValidations.createOrderValidationSchema),
   OrderController.createOrder,
 );
 
