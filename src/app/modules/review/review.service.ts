020da44939c47916a8ae87c801417794f9c08fdf
manik <maniksarker652@gmail.com>
2025-03-01 09:28:12 +0600

    reviewer service created

---
+++ src/app/modules/review/review.service.ts
@@ -0,0 +1,14 @@
+import { IReview } from './review.interface';
+import Review from './reviewer.model';
+
+const createReview = async (reviewerId: string, payload: IReview) => {
+  const result = await Review.create({ ...payload, reviewer: reviewerId });
+
+  return result;
+};
+
+const ReviewService = {
+  createReview,
+};
+
+export default ReviewService;
