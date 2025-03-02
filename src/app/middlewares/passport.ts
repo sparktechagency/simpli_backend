a97ed477d9fd3e7c8a8cb492ede71258802071a7
manik <maniksarker652@gmail.com>
2025-03-02 15:22:04 +0600

    create passport middleware

---
+++ src/app/middlewares/passport.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+import { User } from '../modules/user/user.model';
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL!,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            email: profile.emails?.[0]?.value,
+            // name: profile.displayName,
+            // profilePic: profile.photos?.[0]?.value,
+          });
+          await user.save();
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error, null as any);
+      }
+    },
+  ),
+);
