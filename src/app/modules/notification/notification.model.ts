ded49764b98e0d5db66b98945735103fe0e916f3
manik <maniksarker652@gmail.com>
2025-03-03 08:39:54 +0600

    creae schema for notification

--- src/app/modules/notification/notification.model.ts
+++ src/app/modules/notification/notification.model.ts
@@ -1,31 +1,60 @@
-import { model, Schema } from 'mongoose';
+import mongoose, { Schema, Model } from 'mongoose';
 import { INotification } from './notification.interface';
+import {
+  ENUM_NOTIFICATION_TYPE,
+  ENUM_RECEIVER_TYPE,
+  ENUM_SENDER_TYPE,
+} from '../../utilities/enum';
 
-const notificationSchema = new Schema<INotification>(
+const NotificationSchema = new Schema<INotification>(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    message: {
-      type: String,
-      required: true,
-    },
-    seen: {
-      type: Boolean,
-      default: false,
-    },
     receiver: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: 'userType',
+    },
+    receiverType: {
       type: String,
+      enum: Object.values(ENUM_RECEIVER_TYPE),
       required: true,
     },
+    sender: {
+      type: Schema.Types.ObjectId,
+      refPath: 'senderType',
+      required: false,
+    },
+    senderType: {
+      type: String,
+      enum: Object.values(ENUM_SENDER_TYPE),
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: Object.values(ENUM_NOTIFICATION_TYPE),
+      required: true,
+    },
+    message: { type: String, required: true },
+    data: {
+      commentId: { type: Schema.Types.ObjectId, ref: 'Comment' },
+      orderId: { type: Schema.Types.ObjectId, ref: 'Order' },
+      reviewId: { type: Schema.Types.ObjectId, ref: 'Review' },
+      amount: { type: Number },
+      product: {
+        id: { type: Schema.Types.ObjectId, ref: 'Product' },
+        name: { type: String },
+        quantity: { type: Number },
+      },
+    },
+    isRead: { type: Boolean, default: false },
   },
-
-  {
-    timestamps: true,
-  },
+  { timestamps: true },
 );
 
-const Notification = model<INotification>('Notification', notificationSchema);
+NotificationSchema.index({ receiver: 1, isRead: 1, createdAt: -1 });
+
+const Notification: Model<INotification> = mongoose.model<INotification>(
+  'Notification',
+  NotificationSchema,
+);
 
 export default Notification;
