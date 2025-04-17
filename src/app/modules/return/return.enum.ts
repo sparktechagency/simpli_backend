0616db0d9707d54cdb9903995d177241d9d5f8a2
manik <maniksarker652@gmail.com>
2025-04-17 09:18:26 +0600

    create interface for return

---
+++ src/app/modules/return/return.enum.ts
@@ -0,0 +1,9 @@
+export const ENUM_RETURN_METHOD = {
+  BY_POST: 'BY_POST',
+  SCHEDULE_PICK_UP: 'SCHEDULE_PIC_UP',
+};
+
+export const ENUM_RETURN_STATUS = {
+  PENDING: 'Pending',
+  REFUNDED: 'Refunded',
+};
---
+++ src/app/modules/return/return.interface.ts
@@ -0,0 +1,14 @@
+import { Types } from 'mongoose';
+import { IOrderItem } from '../order/order.interface';
+import { ENUM_RETURN_METHOD, ENUM_RETURN_STATUS } from './return.enum';
+
+export interface IReturn {
+  bussiness: Types.ObjectId;
+  reviewer: Types.ObjectId;
+  items: IOrderItem[];
+  orderId: Types.ObjectId;
+  transactionId: string;
+  totalReturnAmount: number;
+  returnMethod: (typeof ENUM_RETURN_METHOD)[keyof typeof ENUM_RETURN_METHOD];
+  status: (typeof ENUM_RETURN_STATUS)[keyof typeof ENUM_RETURN_STATUS];
+}
