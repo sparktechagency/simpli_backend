0b05ffdab766041c82eb1ae89d95ce1e0658c4fc
manik <maniksarker652@gmail.com>
2025-04-17 09:56:34 +0600

    create service function for get all return

--- src/app/modules/return/return.service.ts
+++ src/app/modules/return/return.service.ts
@@ -1,3 +1,4 @@
+import QueryBuilder from '../../builder/QueryBuilder';
 import { IReturn } from './return.interface';
 import Return from './return.model';
 
@@ -6,8 +7,31 @@ const createReturn = async (reviewerId: string, payload: IReturn) => {
   return result;
 };
 
+const getAllReturn = async (
+  profileId: string,
+  query: Record<string, unknown>,
+) => {
+  const returnQuery = new QueryBuilder(
+    Return.find({ $or: [{ reviewer: profileId }, { bussiness: profileId }] }),
+    query,
+  )
+    .search([''])
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const meta = await returnQuery.countTotal();
+  const result = await returnQuery.modelQuery;
+  return {
+    meta,
+    result,
+  };
+};
+
 const ReturnService = {
   createReturn,
+  getAllReturn,
 };
 
 export default ReturnService;
