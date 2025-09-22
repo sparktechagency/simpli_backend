bb76c624228ab8a1711d522cb9ccdfa3d9c4e2d8
manik <maniksarker652@gmail.com>
2025-09-22 10:25:48 +0600

    create service funtion fro handle withdraw for reviewer and business

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,1 +1,31 @@
-const withDraw;
+import { JwtPayload } from 'jsonwebtoken';
+import AppError from '../../error/appError';
+import Bussiness from '../bussiness/bussiness.model';
+import Reviewer from '../reviewer/reviewer.model';
+import { USER_ROLE } from '../user/user.constant';
+
+const makeWithDraw = async (userData: JwtPayload, amount: number) => {
+  if (userData.role == USER_ROLE.bussinessOwner) {
+    const business = await Bussiness.findById(userData.profileId);
+    if (!business) {
+      throw new AppError(404, 'Business not found');
+    }
+    if (business.currentBalance < amount) {
+      throw new AppError(400, 'Insufficient balance');
+    }
+  } else {
+    const reviewer = await Reviewer.findById(userData.profileId);
+    if (!reviewer) {
+      throw new AppError(404, 'Reviewer not found');
+    }
+    if (reviewer.currentBalance < amount) {
+      throw new AppError(400, 'Insufficient balance');
+    }
+  }
+};
+
+const PaymentService = {
+  makeWithDraw,
+};
+
+export default PaymentService;
