8effd98f7c1f237ce67d4566f0e084ec967b4a08
manik <maniksarker652@gmail.com>
2026-02-08 14:45:51 +0600

    try to fix

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -129,7 +129,10 @@ const createReview = async (reviewerId: string, payload: any) => {
     const videoId = `${Date.now()}-${Math.random()
       .toString(36)
       .substring(2, 8)}`;
-    const rawFileName = payload.video.split('/').pop()?.replace('.mp4', '');
+    const rawFileName = payload.video
+      .split('/')
+      .pop()!
+      .replace(/\.[^/.]+$/, '');
 
     const job = await createHlsJobFromUrl({
       videoUrl: payload.video,
