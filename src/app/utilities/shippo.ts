d50f3134ed08d27d92e01fdab498d234322cd743
manik <maniksarker652@gmail.com>
2026-02-17 16:02:38 +0600

    fix shippo issue

--- src/app/utilities/shippo.ts
+++ src/app/utilities/shippo.ts
@@ -1,5 +1,8 @@
 import { Shippo } from 'shippo';
 import config from '../config';
+
+console.log('Shippo API Key:', config.shippo.api_key);
+
 const shippo = new Shippo({ apiKeyHeader: config.shippo.api_key as string });
 
 export default shippo;
