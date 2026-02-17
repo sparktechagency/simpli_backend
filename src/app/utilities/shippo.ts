c10e0df0f3d3e670aa4b001c128658ef74279f5d
manik <maniksarker652@gmail.com>
2026-02-17 16:55:28 +0600

    make changes

--- src/app/utilities/shippo.ts
+++ src/app/utilities/shippo.ts
@@ -2,8 +2,6 @@
 import { Shippo } from 'shippo';
 import config from '../config';
 
-console.log('Shippo API Key:', config.shippo.api_key);
-
 if (!config.shippo.api_key) {
   throw new Error('Shippo API key is not defined in the configuration.');
 }
